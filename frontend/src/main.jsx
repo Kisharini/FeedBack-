57f45ca44073239c3d7efe10b2c652d737e9139d
Kisharini <kisharinijeevanathan07@gmail.com>
2026-06-21 14:48:01 +0800

    update merge conflict

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './srcApp.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "leaflet/dist/leaflet.css";
@@ -22,4 +10,3 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
->>>>>>> 822f7ce03a154547be32d378797ba9d7209f164f
